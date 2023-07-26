317792a64498189795436395c7e32b348ea3808f
Kashishvarmaa <136305941+Kashishvarmaa@users.noreply.github.com>
2023-07-26 17:11:26 +0530

    DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
-
-    constructor() ERC20("Degen", "DGN") {}
-
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
-    }
+contract DegenToken is ERC20, ERC20Burnable, Ownable {
+string public shop;
+    constructor() ERC20("Degen", "DGN") {
+shop = "Have an amazing time shopping :) Items available are: 1. Degen sweatshirt 2.Degen cap 3. Degen Pendent 4. Degen Rings";
 }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+function redeem(uint256 red_amt) public {
+    require(balanceOf(msg.sender) >= 100, "You need more balance to redeem");
+    require(red_amt > 0 && red_amt <= 4, "Redeem amount invalid");
+
+    uint256 final_price = red_amt * 120;
+    _burn(msg.sender, final_price);
+}}
